275614b6bfead5632c5b9382f3951afc66acde3e
Samuel Pfrommer <sam.pfrommer@gmail.com>
2017-05-19 23:33:36 -0400

    Implement trapezoidal quadrature for actuation squared cost
    Fixes "uptick" bug at beginning and end of phases

--- src/cost/actsqrcost.m
+++ src/cost/actsqrcost.m
@@ -1,9 +1,24 @@
-function [ cost ] = actsqrcost( funparams, params )
+function [ cost ] = actsqrcost( funParams, ntoParams )
     % Unpack the vector
     [stanceT, ~, ~, ~, ~, ~, ~, ~, ~, ~, raddot, torque] = ...
-        unpack(funparams, params);
+        unpack(funParams, ntoParams);
+    phasen = size(ntoParams.phases, 1);
+    
+    dts = kron(stanceT./ntoParams.gridn, ones(ntoParams.gridn-1, 1));
+    
+    startRemInd = 1 : ntoParams.gridn : ntoParams.gridn * phasen;
+    endRemInd = ntoParams.gridn : ntoParams.gridn : ntoParams.gridn * phasen;
+    
+    [torqueA, torqueB] = deal(torque);
+    torqueA(startRemInd) = [];
+    torqueB(endRemInd) = [];
+    torqueCost = sum(0.5 .* dts .* (torqueA.^2 + torqueB.^2));
 
-    dts = kron(stanceT./params.gridn, ones(params.gridn, 1));
-    cost = sum((raddot .* dts) .^ 2) + sum((torque .* dts) .^ 2);
+    [raddotA, raddotB] = deal(raddot);
+    raddotA(startRemInd) = [];
+    raddotB(endRemInd) = [];
+    raddotCost = sum(0.5 .* dts .* (raddotA.^2 + raddotB.^2));
+
+    cost = torqueCost + raddotCost;
 end
 
