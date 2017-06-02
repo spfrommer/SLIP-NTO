eb79470f8badce91c5a853eb08204e31fbd1d851
Samuel Pfrommer <sam.pfrommer@gmail.com>
2017-06-01 22:31:11 -0400

    Allow optional supplying of initial guess to optimize

--- src/ntofuns/optimize.m
+++ src/ntofuns/optimize.m
@@ -1,10 +1,10 @@
-function [ optimal, cost, flag ] = optimize( params )
+function [ optimal, cost, flag ] = optimize( ntoParams, initialGuess )
     disp('Slip patch: ');
-    disp(params.slipPatch);
+    disp(ntoParams.slipPatch);
     disp('Initial state: ');
-    disp(params.initialState);
+    disp(ntoParams.initialState);
     disp('Final x: ');
-    disp(params.finalProfileX);
+    disp(ntoParams.finalProfileX);
 
     % Options for optimizing the constant cost function
     cOptions = optimoptions(@fmincon, 'TolFun', 0.00000001, ...
@@ -36,52 +36,57 @@ function [ optimal, cost, flag ] = optimize( params )
     Aeq = [];
     Beq = [];
     % Set up the bounds
-    [lb, ub] = bounds(params);
+    [lb, ub] = bounds(ntoParams);
 
-    numVars = size(params.phases, 1) * 2 - 1 + params.gridn * size(params.phases, 1) * 10;
+    numVars = size(ntoParams.phases, 1) * 2 - 1 + ntoParams.gridn * size(ntoParams.phases, 1) * 10;
     funparams = conj(sym('x', [1 numVars], 'real')');
     
     fprintf('Generating constraints...\n');
-    [c, ceq] = constraints(funparams, params);
+    [c, ceq] = constraints(funparams, ntoParams);
     cjac = jacobian(c, funparams).';
     ceqjac = jacobian(ceq, funparams).';
     fprintf('Generating constraints function...\n');
     constraintsFun = matlabFunction(c, ceq, cjac, ceqjac, 'Vars', {funparams});
     
     fprintf('Generating costs...\n');
-    ccost = constcost(funparams, params);
+    ccost = constcost(funparams, ntoParams);
     ccostjac = jacobian(ccost, funparams).';
     ccostFun = matlabFunction(ccost, ccostjac, 'Vars', {funparams});
 
-    acost = actsqrcost(funparams, params);
+    acost = actsqrcost(funparams, ntoParams);
     acostjac = jacobian(acost, funparams).';
     acostFun = matlabFunction(acost, acostjac, 'Vars', {funparams});
     
-    awcost = actworkcost(funparams, params);
+    awcost = actworkcost(funparams, ntoParams);
     awcostjac = jacobian(awcost, funparams).';
     awcostFun = matlabFunction(awcost, awcostjac, 'Vars', {funparams});
     
-    flag = -1;
-    tryCount = 0;
-    while flag < 0 && tryCount < 3
-        fprintf('Generating initial possible trajectory (%d)...\n', tryCount+1);
-        % Generate initial guess
-        x0 = MinMaxCheck(lb, ub, ones(numVars, 1) * rand() * 2);
-        [ci, ceqi, cjaci, ceqjaci] = constraintsFun(x0);
-        while any(imag(ci))  || any(imag(ceqi))  || any(any(imag(cjaci)))  || any(any(imag(ceqjaci)))  || ...
-              any(isnan(ci)) || any(isnan(ceqi)) || any(any(isnan(cjaci))) || any(any(isnan(ceqjaci))) || ...
-              any(isinf(ci)) || any(isinf(ceqi)) || any(any(isinf(cjaci))) || any(any(isinf(ceqjaci)))
-            fprintf('Regenerating initial guess...\n');
-            x0 = MinMaxCheck(lb, ub, ones(numVars, 1) * rand());
+    if nargin < 2
+        flag = -1;
+        tryCount = 0;
+        while flag < 0 && tryCount < 3
+            fprintf('Generating initial possible trajectory (%d)...\n', tryCount+1);
+            % Generate initial guess
+            x0 = MinMaxCheck(lb, ub, ones(numVars, 1) * rand() * 2);
             [ci, ceqi, cjaci, ceqjaci] = constraintsFun(x0);
+            while any(imag(ci))  || any(imag(ceqi))  || any(any(imag(cjaci)))  || any(any(imag(ceqjaci)))  || ...
+                  any(isnan(ci)) || any(isnan(ceqi)) || any(any(isnan(cjaci))) || any(any(isnan(ceqjaci))) || ...
+                  any(isinf(ci)) || any(isinf(ceqi)) || any(any(isinf(cjaci))) || any(any(isinf(ceqjaci)))
+                fprintf('Regenerating initial guess...\n');
+                x0 = MinMaxCheck(lb, ub, ones(numVars, 1) * rand());
+                [ci, ceqi, cjaci, ceqjaci] = constraintsFun(x0);
+            end
+
+            fprintf('########## Finding feasible trajectory ##########\n');
+            % Find any feasible trajectory
+            [feasible, ~, flag, ~] = ...
+                fmincon(@(x) call(ccostFun, x, 2),x0,A,B,Aeq,Beq,lb,ub, ...
+                        @(x) call(constraintsFun, x, 4), cOptions);
+            tryCount = tryCount + 1;
         end
-    
-        fprintf('########## Finding feasible trajectory ##########\n');
-        % Find any feasible trajectory
-        [feasible, ~, flag, ~] = ...
-            fmincon(@(x) call(ccostFun, x, 2),x0,A,B,Aeq,Beq,lb,ub, ...
-                    @(x) call(constraintsFun, x, 4), cOptions);
-        tryCount = tryCount + 1;
+    else
+        feasible = initialGuess;
+        flag = 1;        
     end
 
     if flag > 0
